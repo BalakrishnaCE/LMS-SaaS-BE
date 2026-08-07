2a3af9d5b7e86d905dd1bb76f86e9357155eeb70
yamini-chandragiri <yamini.c@noveloffice.in>
2026-08-07 17:57:07 +0530

    created all the docs which we planned for LMS saas

---
+++ lms/lms/doctype/lms_content/test_lms_content.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Novel Office and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLMSContent(FrappeTestCase):
+	pass
--- lms/lms/doctype/lms_permission/lms_permission.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Novel Office and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class LMSPermission(Document):
-	pass
